42d36fc5c961dda6d89e74da3dbc40456343bb7e
Stan <skarundi@skarundi-t61p-laptop.(none)>
2009-01-21 08:38:07 -0500

    Add 2 things. 1) specify gems required to get this app to work. 2) generate random key each time the rails app is started.

--- config/environment.rb
+++ config/environment.rb
@@ -20,15 +20,16 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate'
 
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -52,11 +53,18 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
+  secret_file = File.join(RAILS_ROOT, 'secret')
+  if File.exist?(secret_file)
+        secret = File.read(secret_file)
+  else
+        secret = ActiveSupport::SecureRandom.hex(64)
+        File.open(secret_file, 'w') { |f| f.write(secret) }
+  end
   config.action_controller.session = {
     :session_key => '_ajaxtable_session',
-    :secret      => '4ff80c9bb26295c6580933ca10bad335d4d2f3d4904e9affb6bbed7629b9dc8b9bc6b2b596553e08ee61fa2a8c37e7cfce3fdcf367cc49196ea0e97fad570fc9'
+    :secret      => secret
   }
 
   # Use the database for sessions instead of the cookie-based default,
